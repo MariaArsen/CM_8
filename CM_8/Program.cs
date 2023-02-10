8bbb9ad4d82d3743efc7c5a0e84b37badab0667b
Солнце <@LAPTOP-DPKNJGCI>
2023-02-10 13:28:00 +0300

    Добавьте файлы проекта.

---
+++ CM_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CM_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Temp";
+            string[] files = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            string[] files2 = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (string file2 in files2)
+            {
+                Console.WriteLine(file2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Temp\Тигр.txt";
+            string line;
+            int l = 0;  //Количество строк
+            StreamReader sr = new StreamReader(path);
+            while ((line=sr.ReadLine())!=null)
+            {
+                l++;
+            }
+            string word;
+            int s = 0; //Количество слов
+            foreach (string word in sr.ReadLine())
+            {
+                Convert.ToString(sr.ReadLine());
+                s++;
+            }
+            Console.WriteLine("Количество строк в тексе {0}", l);
+            Console.WriteLine("Количество слов в тексе {0}", s);
+            Console.WriteLine("Количество символов в тексе {0}", sr.Lengt);
+            Console.ReadKey();
+        }
+    }
+}
